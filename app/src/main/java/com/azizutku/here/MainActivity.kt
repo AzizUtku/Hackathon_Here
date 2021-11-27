fc60a0ecef8c19c4a0e0e745d8c4f3432e70326f
Aziz Utku Kağıtcı <azizutku@gmail.com>
2021-11-27 10:45:54 +0300

    Add sign in

--- app/src/main/java/com/azizutku/here/MainActivity.kt
+++ app/src/main/java/com/azizutku/here/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.azizutku.here
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.azizutku.here.databinding.ActivityMainBinding
@@ -14,6 +18,10 @@ import com.azizutku.here.vo.DataState
 import com.google.firebase.auth.AuthResult
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import android.view.WindowManager
+
+
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -31,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         subscribeObservers()
         setupNavControllers()
 
-        authViewModel.login("azizutku@gmail.com", "12345678a")
+        authViewModel.getUserFromCache()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -48,13 +56,9 @@ class MainActivity : AppCompatActivity() {
             when (dataState) {
                 is DataState.Success<AuthResult> -> {
                     authResult = dataState.data
-                    Timber.i("Userr: ${dataState.data.user?.email}")
                 }
                 is DataState.Error -> {
-                    // no op
-                }
-                is DataState.Loading -> {
-                    // no op
+                    navController.navigate(R.id.action_roomsFragment_to_signInFragment)
                 }
                 else -> {
                     // no op
@@ -86,6 +90,11 @@ class MainActivity : AppCompatActivity() {
 
         }
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    fun updateStatusBarColor(color: Int) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = color
     }
 
 }
--- app/src/main/java/com/azizutku/here/presentation/auth/SignInFragment.kt
+++ app/src/main/java/com/azizutku/here/presentation/auth/SignInFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.azizutku.here.MainActivity
 import com.azizutku.here.R
 
 class SignInFragment : Fragment() {
@@ -20,6 +22,9 @@ class SignInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (requireActivity() as MainActivity).updateStatusBarColor(
+            ContextCompat.getColor(requireContext(), R.color.teal_700)
+        )
         return inflater.inflate(R.layout.sign_in_fragment, container, false)
     }
 
